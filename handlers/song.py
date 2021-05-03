5f39a26caec2f77f488b00fd008be1fa88b03800
KingPrabhasha <78271696+KingPrabhasha@users.noreply.github.com>
2021-05-04 02:35:44 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -39,7 +39,7 @@ import time
 from helpers.errors import DurationLimitError
 
 @Client.on_message(filters.command('s') & ~filters.private & ~filters.channel)
-def song(client, message):
+def s(client, message):
 
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
@@ -78,7 +78,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Requested By :** ' {rpk}
+        rep = '**🎵 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝙗𝙮 @GalaxyLanka •••**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
